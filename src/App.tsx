12e1be9f93bfc0068eccb5850d2200af5fc6fbc5
YossiBenZaken <yossibz@gmail.com>
2022-01-18 18:03:57 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,75 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { MenuButton } from "./components/MenuButton";
+import { Menu } from "./models/Menu";
 
 function App() {
-
+  const [menus, setMenus] = useState([
+    {
+      menus: [
+        {
+          menus: [
+            {
+              menus: [
+                {
+                  menus: [],
+                  text: "Submenu 1",
+                },
+                {
+                  menus: [],
+                  text: "Submenu 1",
+                },
+              ],
+              text: "Menu 1-1-1",
+            },
+            {
+              menus: [],
+              text: "Menu 1-1-2",
+            },
+          ],
+          text: "menu1",
+        },
+        {
+          menus: [],
+          text: "menu2",
+        },
+      ],
+      text: "Menu 1",
+    },
+    {
+      menus: [
+        {
+          menus: [],
+          text: "Menu 2",
+        },
+      ],
+      text: "Menu 2",
+    },
+    {
+      menus: [],
+      text: "Menu 3",
+    },
+    {
+      menus: [],
+      text: "Menu 4",
+    },
+  ]);
   return (
     <div className="App">
+      <h1>Simple Menu Test</h1>
+      <div className="navBar">
+        <div>Home</div>
+        {menus.map((menu: Menu, index: number) => (
+          <MenuButton
+            key={index}
+            menus={menu.menus}
+            text={menu.text}
+            submenu={false}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Menu } from "../models/Menu";
+import "./MenuButton.css";
+interface Props {
+  menus: Menu[];
+  text: string;
+  submenu: boolean;
+}
+
+export const MenuButton = (props: Props) => {
+    const [open,setOpen] = useState(false);
+  return (
+    <div className="menu" onMouseEnter={() => setOpen((prev) => !prev)} onMouseLeave={() => setOpen(false)}>
+      {props.text}
+      <div className={`${props.submenu ? 'subMenu' : 'menuIn'} ${open ? 'open': ''}`} onMouseDown={() => setOpen((prev) => !prev)}>
+        {props.menus.map((menu: Menu, index: number) => (
+          <MenuButton key={index} menus={menu.menus} text={menu.text} submenu={props.menus.length > 0} />
+        ))}
+      </div>
+    </div>
+  );
+};
